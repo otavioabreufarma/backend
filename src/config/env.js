6fb9d4b0b2a9fa22ed56e8b6321e9c1572b3b53c
otavioabreufarma <otavioabreufarma@gmail.com>
2026-02-08 00:09:06 -0300

    Update environment configuration values

--- src/config/env.js
+++ src/config/env.js
@@ -1,14 +1,14 @@
 export default {
   PORT: 3000,
-  BASE_URL: "https://seu-backend.com",
-  STEAM_API_KEY: "STEAM_API_KEY",
-  STEAM_REALM: "https://seu-backend.com",
+  BASE_URL: "https://backend-fhg1.onrender.com",
+  STEAM_API_KEY: "DC8C7F81B10230B8D616DB9E6AB96D3D",
+  STEAM_REALM: "https://backend-fhg1.onrender.com",
   STEAM_RETURN_URL: "https://seu-backend.com/auth/steam/callback",
 
-  INFINITEPAY_HANDLE: "SEU_INFINITETAG",
+  INFINITEPAY_HANDLE: "otavioabreupix",
 
-  INTERNAL_DISCORD_TOKEN: "TOKEN_DISCORD_INTERNO",
-  INTERNAL_RUST_TOKEN: "TOKEN_RUST_INTERNO",
+  INTERNAL_DISCORD_TOKEN: "a7f3c9d2e6b5a8c1f4d7e9b0c2a1f8e4",
+  INTERNAL_RUST_TOKEN: "e4a7c9b2f1d3a6c8e9b0d5f7a4c2e1f6",
 
-  SESSION_SECRET: "SESSION_SUPER_SEGURA"
+  SESSION_SECRET: "m8F4kXqZk6A3nN1Zz9Qp2YtR0bEJHcW6dF0Sx7LqP9M="
 };
